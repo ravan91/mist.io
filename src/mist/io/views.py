ac7ba10c144ce553bbb4e1ce89c85571c32e74bc
commixon <commixon@gmail.com>
2013-09-23 11:18:52 +0300

    Normalize the names of the default images, they look better now

--- src/mist/io/views.py
+++ src/mist/io/views.py
@@ -818,10 +818,10 @@ def list_images(request):
                     images.append(i)
                     
             for image in images:
-                image.name = EC2_IMAGES[conn.type][image.id]
-                if not image.name:
-                    image.name = EC2_IMAGES[conn.type].get(image.id, image.id)
-                
+                #image.name = EC2_IMAGES[conn.type][image.id]
+                #if not image.name:
+                #    image.name = EC2_IMAGES[conn.type].get(image.id, image.id)
+                image.name = EC2_IMAGES[conn.type].get(image.id, image.name)
         else:
             images = conn.list_images()
     except:
